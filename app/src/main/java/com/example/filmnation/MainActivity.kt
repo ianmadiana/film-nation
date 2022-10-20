7fbe293c28a7cfd1b777db2db566f5b58404cf3c
Ian Madiana <113165220+ianmadiana@users.noreply.github.com>
2022-10-20 22:58:02 +0700

    fix warna

--- app/src/main/java/com/example/filmnation/MainActivity.kt
+++ app/src/main/java/com/example/filmnation/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.filmnation
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.example.filmnation.adapter.ItemAdapter
 import com.example.filmnation.data.Datasource
+import com.example.filmnation.R.*
+import android.view.View
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,4 +26,6 @@ class MainActivity : AppCompatActivity() {
         //meningkatkan performa aplikasi jika mengetahui bahwa perubahan dalam isi tidak mengubah ukuran tata letak RecyclerView
         recyclerView.setHasFixedSize(true)
     }
+
+
 }
